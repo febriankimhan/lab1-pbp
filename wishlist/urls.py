b794a72a7657e88bd85a7f2b0f0d75f2fe0c1fe1
Febrian Dwi Kimhan <febrian.dwi11@ui.ac.id>
2022-10-06 14:38:59 +0700

    lab5

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,17 +1,10 @@
 from django.urls import path
-from wishlist.views import show_wishlist
-from wishlist.views import show_xml 
-from wishlist.views import show_json
-from wishlist.views import show_xml_by_id
-from wishlist.views import show_json_by_id
-from wishlist.views import register
-from wishlist.views import login_user
-from wishlist.views import logout_user
-
+from wishlist.views import *
 app_name = 'wishlist'
 
 urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
+    path('ajax/', show_wishlist_ajax, name='show_wishlist_ajax'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
     path('xml/<int:id>', show_xml_by_id, name='show_xml_by_id'),
